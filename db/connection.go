e23530790ec334d2b195e4084efc617ba441bc2a
black-gato <tonymirville@gmail.com>
2023-11-27 16:19:42 -0600

    loading data with init

--- db/connection.go
+++ db/connection.go
@@ -8,7 +8,7 @@ import (
 
 func OpenConnection() (con *sql.DB, err error) {
 
-	db, err := sql.Open("sqlite3", "file:youtube.db?cache=share&mode=rwc&_journal_mode=WAL&synchronous=normal&temp_store=memory&mmap_size=30000000000&page_size=32768")
+	db, err := sql.Open("sqlite3", "file:youtube.db?cache=shared&mode=rwc&_journal_mode=WAL&synchronous=normal&temp_store=memory&mmap_size=30000000000&page_size=32768")
 
 	if err != nil {
 		return nil, err
