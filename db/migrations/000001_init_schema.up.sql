f6d564237b77623829875e5e4de557d74c179a70
black-gato <tonymirville@gmail.com>
2023-11-27 20:47:29 -0600

    added new index

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS videos (
+CREATE TABLE videos (
   id TEXT PRIMARY KEY NOT NULL,
   video_type TEXT NOT NULL,
   video_title TEXT NOT NULL,
-  channel_id TEXT NOT NULL,
+  channel_id TEXT,
   FOREIGN KEY(channel_id) REFERENCES channels(id)
 );
-CREATE TABLE IF NOT EXISTS channels (
+CREATE TABLE channels (
   id TEXT PRIMARY KEY NOT NULL,
   channel_name TEXT NOT NULL,
   channel_url TEXT NOT NULL UNIQUE,
-  is_subbed BOOLEAN NOT NULL
+  is_subbed INTEGER NOT NULL
 );
-CREATE TABLE IF NOT EXISTS watch_history (
+CREATE TABLE watch_history (
   id INTEGER PRIMARY KEY,
   video_id TEXT,
   watched_at TEXT NOT NULL,
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS watch_history (
   FOREIGN KEY(channel_id) REFERENCES channels(id),
   FOREIGN KEY(video_id) REFERENCES videos(id)
 );
-CREATE INDEX _25be8e9c140a414d88cae1490ca9cc77 ON videos (video_type);
-CREATE INDEX _c4d70e462d114758899d737ef0304a59 ON channels (is_subbed);
-CREATE INDEX _617adedd5c104cf6bd5020be36aed4e4 ON watch_history (video_id);
+CREATE INDEX _e2db485ef680414f9216cd60f84ae0c5 ON videos (video_type);
+CREATE INDEX _cd5c39677ae24c20b90cc1510bbb0ef6 ON channels (is_subbed);
+CREATE INDEX _77074f8c135647038a7ab94d6bfa14bf ON watch_history (video_id);
+CREATE INDEX _dae0b1c768dd470c9f475aef92f93f14 ON watch_history (channel_id);
